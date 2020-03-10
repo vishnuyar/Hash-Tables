921edd6f7039aa92387d1d2e3bcfa9145ab1112e
vishnuyar <vardhanvishnu@gmail.com>
2020-03-11 02:03:07 +0530

    hash implementation

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,11 +1,15 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
+    def __str__(self):
+        return f'{self.key}+{self.value}'
 
 class HashTable:
     '''
@@ -23,6 +27,7 @@ class HashTable:
 
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
+         
         return hash(key)
 
 
@@ -51,7 +56,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        node = self.storage[index]
+        if node is None:
+            node = LinkedPair(key,value)
+        else:
+            prev = node
+            while node.next:
+                node = node.next
+            node.next = LinkedPair(key,value)
+        
 
 
 
@@ -63,8 +77,9 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        index = self._hash_mod(key)
+        if self.storage[index]:
+            self.storage[index] = None
 
     def retrieve(self, key):
         '''
@@ -74,8 +89,19 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        index = self._hash_mod(key)
+        print('index',index,self.storage[index])
+        if self.storage[index]:
+            while self.storage[index]:
+                print("ff",index,self.storage[index].key,key)
+                if self.storage[index].key == key:
+                    return self.storage[index].value
+                else:
+                    self.storage[index] = self.storage[index].next
+            return None
+        else:
+            return None
+        
 
     def resize(self):
         '''
@@ -84,7 +110,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        old_capacity = self.capacity
+        old_storage = self.storage.copy()
+        self.capacity = old_capacity * 2
+        self.storage = [None] * self.capacity
+        for i in range(old_capacity):
+            print('cont',i)
+            while old_storage[i]:
+                print('p',i,old_storage[i].key,old_storage[i].value)
+                self.insert(old_storage[i].key,old_storage[i].value)
+                old_storage[i] = old_storage[i].next
 
 
 
@@ -101,7 +136,7 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
-
+    
     # Test resizing
     old_capacity = len(ht.storage)
     ht.resize()
